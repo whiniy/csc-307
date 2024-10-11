d65d609d361d0fe29cba103e10845551f356b4b7
whiniy <winnie.t835@gmail.com>
2024-10-11 12:17:04 -0700

    your commit msg

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,31 +1,42 @@
-// src/MyApp.jsx
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Table from "./Table";
-import Form from "./Form";
 
 function MyApp() {
-	const [characters, setCharacters] = useState([]);
+        const [characters, setCharacters] = useState ([
+                {
+                        name: "Charlie",
+                        job: "Janitor"
+                },
+                {
+                        name: "Mac",
+                        job: "Bouncer"
+                },
+                {
+                        name: "Dee",
+                        job: "Aspiring actress"
+                },
+                {
+                        name: "Dennis",
+                        job: "Bartender"
+                }
+        ]);
 
-  	function removeOneCharacter(index) {
-    		const updated = characters.filter((character, i) => {
-      			return i !== index;
-    		});
-    		setCharacters(updated);
-  	}
+        function removeOneCharacter(index) {
+                const updated = characters.filter((character, i) => {
+                        return i !== index;
+                });
+                setCharacters(updated);
+        }
 
-	function updateList(person) {
-  		setCharacters([...characters, person]);
-	}
-
-	return (
-  		<div className="container">
-    			<Table
-      				characterData={characters}
-      				removeCharacter={removeOneCharacter}
-    			/>
-    			<Form handleSubmit={updateList} />
-  		</div>
-	);
+        return (
+                <div className="container">
+                        <Table
+                                characterData={characters}
+                                removeCharacter={removeOneCharacter}
+                        />
+                </div>
+        );
 }
 
 export default MyApp;
+
