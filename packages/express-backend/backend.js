1c83a8b7e5626dd6a588a72969b0e2ef59ae2fd9
whiniy <winnie.t835@gmail.com>
2024-10-11 23:20:38 -0700

    step 6

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -54,6 +54,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
 	users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+	users["users_list"].push(user);
+	return user;
+};
+
 app.get("/users", (req, res) => {
 	const name = req.query.name;
 	if (name != undefined) {
@@ -75,4 +80,9 @@ app.get("/users/:id", (req, res) => {
 	}
 });
 
+app.post("/users", (req, res) => {
+	const userToAdd = req.body;
+	addUser(userToAdd);
+	res.send();
+});
 
